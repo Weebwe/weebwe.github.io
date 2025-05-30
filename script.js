8a9646c868ce43da6f657bdbeda38768d6736388
Weebwe LLC <weeb.llc@gmail.com>
2025-05-30 05:21:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-// Ваш Firebase конфігураційний об'єкт
-const firebaseConfig = {
-  apiKey: "AIzaSyCs449Tb9Oi8xBXzC64zIRgJhY9S3rQKTM",
-  authDomain: "weebwe-llc.firebaseapp.com",
-  projectId: "weebwe-llc",
-  storageBucket: "weebwe-llc.firebasestorage.app",
-  messagingSenderId: "298492598941",
-  appId: "1:298492598941:web:8dd5b5d27ca9116b251274",
-  measurementId: "G-B1MGB8RVQL"
-};
+// script.js
+import { firebaseConfig } from './firebase-config.js'; // Імпорт конфігурації
 
 // Імпортуємо функції з Firebase SDK v9+
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 
 // Ініціалізуємо Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); // Тепер firebaseConfig імпортується
 const db = getDatabase(app);
 
 let currentLanguage = 'uk'; // Мова за замовчуванням завжди українська
