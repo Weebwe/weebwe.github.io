fd5c03672ad529d4677e2330c62c70a6192d1f18
Weebwe LLC <weeb.llc@gmail.com>
2025-05-30 05:19:50 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// Цей файл містить ваші конфігураційні дані Firebase.
+// НЕ ЗАВАНТАЖУЙТЕ ЙОГО НА ГІТХАБ!
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCs449Tb9Oi8xBXzC64zIRgJhY9S3rQKTM", // Ваш API Key
+  authDomain: "weebwe-llc.firebaseapp.com",
+  projectId: "weebwe-llc",
+  storageBucket: "weebwe-llc.firebasestorage.app",
+  messagingSenderId: "298492598941",
+  appId: "1:298492598941:web:8dd5b5d27ca9116b251274",
+  measurementId: "G-B1MGB8RVQL"
+};
